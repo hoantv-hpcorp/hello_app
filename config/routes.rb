b591bb26555206804aeadab90e064efb94710ea8
Tran Hoan <tranvanhoank49d@gmail.com>
2015-10-27 22:45:44 +0700

    Add stylesheet for page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -16,7 +18,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root	'users#index'
+  root	'static_pages#home'
+  get	'help'    =>	'static_pages#help'
+  get	'about'		=>	'static_pages#about'
+  get	'contact'	=>	'static_pages#contact'
+  get	'signup'		=>	'users#new'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027071054) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
