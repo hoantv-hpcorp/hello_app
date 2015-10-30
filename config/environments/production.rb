71c1f1e137c6606a6f2b1cdc396f77a8bc4df980
hoantv-vntech <hoantv@vnpt-technology.vn>
2015-10-30 16:36:43 +0700

    Add password resets & email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               Rails.application.secrets.domain_name,
+    user_name:            'noreply.hpcorp',
+    password:             'Abcdef@123',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   get 'users/new'
-
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   get 'static_pages/about'
-  
   get 'static_pages/contact'
-  
   resources :microposts
-
   resources :users
-  
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
