f734c0311ec995c6eb92343370291c1d6d167104
hoantv-vntech <hoantv@vnpt-technology.vn>
2015-10-30 17:13:51 +0700

    Add password resets & email configuration + Edit mail server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,14 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               Rails.application.secrets.domain_name,
+    user_name:            'noreply.hpcorp',
+    password:             'Abcdef@123',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
