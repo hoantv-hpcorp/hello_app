e938926d63270386d4f1ac6584019cd3ae2845cf
hoantv-vntech <hoantv@vnpt-technology.vn>
2015-10-30 14:01:02 +0700

    Add account activations

--- db/migrate/20151029115855_add_admin_to_users.rb
+++ db/migrate/20151029115855_add_admin_to_users.rb
@@ -1,12 +1,5 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
     add_column :users, :admin, :boolean
-    
-    User.create(
-      name: "admin", 
-      email: "hoantv.hpcorp@gmail.com", 
-      password: "Abcdef123", 
-      password_confirmation: "Abcdef123", 
-      admin: true)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Admin",
+             email: "hoantv.hpcorp@gmail.com",
+             password:              "Abcdef123",
+             password_confirmation: "Abcdef123",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
-
-99.times do |n|
+             password:              "abcdef",
+             password_confirmation: "abcdef",
+             activated: true,
+             activated_at: Time.zone.now)
+           
+50.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
